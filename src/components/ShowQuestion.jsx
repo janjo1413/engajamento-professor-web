851ccf65637f2d4d315b16e5afebefbf3acf4c90
Guilherme Santos <santosgui678@gmail.com>
2024-10-19 10:55:18 -0300

    Adicionada opcao de cancelar questionario

--- src/components/ShowQuestion.jsx
+++ src/components/ShowQuestion.jsx
@@ -3,7 +3,7 @@ import Typography from '@mui/material/Typography';
 import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 import api from '../services/api';
 import { Button, CircularProgress, IconButton, Paper, Stack } from "@mui/material";
-import { RemoveRedEye, VisibilityOff, Close } from '@mui/icons-material';
+import { RemoveRedEye, VisibilityOff } from '@mui/icons-material';
 import { useQuizClass } from '../contexts/QuizClassContext';
 
 export default function ShowQuestion({ onFinishQuiz }) {
@@ -80,49 +80,59 @@ export default function ShowQuestion({ onFinishQuiz }) {
       <Typography variant="h5">Questão {currentQuestionIndex + 1} de {quiz.questoes.length}</Typography>
       <Typography variant="h5">{quiz.questoes[currentQuestionIndex].enunciado}</Typography>
       <Typography variant="h5">Responda <span style={{ color: '#1E90FF' }}>Verdadeiro</span> ou <span style={{ color: '#FF4500' }}>Falso</span></Typography>
-      <Typography variant="h5">Resposta correta</Typography>
 
-      {
-        visible ?
-          <IconButton onClick={() => setVisible(!visible)} disabled={!timeIsOver}>
-            <VisibilityOff />
-          </IconButton>
+      {timeIsOver && (
+        <Stack spacing={4}
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}>
+          <Typography variant="h5">Resposta correta</Typography>
 
-          :
+          {
+            visible ?
+              <IconButton onClick={() => setVisible(!visible)} disabled={!timeIsOver}>
+                <VisibilityOff />
+              </IconButton>
 
-          <IconButton onClick={() => setVisible(!visible)} disabled={!timeIsOver}>
-            <RemoveRedEye />
-          </IconButton>
-
-      }
-
-      {
-        visible ?
-          (
-            quiz.questoes[currentQuestionIndex].resposta === 'V' ?
-              <Typography variant='h4'>Verdadeiro</Typography>
               :
-              <Typography variant='h4'>Falso</Typography>
-          ) : (
-            <Paper elevation={4} sx={{ p: 4, mx: 'auto', width: 200 }}></Paper>
-          )
-      }
 
-      {
-        showSpinner ? (
-          <CircularProgress />
-        ) : (
-          currentQuestionIndex === quiz.questoes.length - 1 ? (
-            <Button variant="contained" onClick={finalizarQuestionario} disabled={!buttonEnabled}>
-              Finalizar questionário
-            </Button>
-          )
-            : (
-              <Button variant="contained" onClick={liberarProximaQuestao} disabled={!buttonEnabled}>
-                Próxima questão
-              </Button>
+              <IconButton onClick={() => setVisible(!visible)} disabled={!timeIsOver}>
+                <RemoveRedEye />
+              </IconButton>
+
+          }
+
+          {
+            visible ?
+              (
+                quiz.questoes[currentQuestionIndex].resposta === 'V' ?
+                  <Typography variant='h4'>Verdadeiro</Typography>
+                  :
+                  <Typography variant='h4'>Falso</Typography>
+              ) : (
+                <Paper elevation={4} sx={{ p: 4, mx: 'auto', width: 200 }}></Paper>
+              )
+          }
+
+          {
+            showSpinner ? (
+              <CircularProgress />
+            ) : (
+              currentQuestionIndex === quiz.questoes.length - 1 ? (
+                <Button variant="contained" onClick={finalizarQuestionario} disabled={!buttonEnabled} sx={{ mt: 4 }}>
+                  Finalizar questionário
+                </Button>
+              )
+                : (
+                  <Button variant="contained" onClick={liberarProximaQuestao} disabled={!buttonEnabled} sx={{ mt: 4 }}>
+                    Próxima questão
+                  </Button>
+                )
             )
-        )
+          }
+        </Stack>
+      )
       }
     </Stack>
   )
--- src/pages/QuizApply.jsx
+++ src/pages/QuizApply.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button, Container, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, Snackbar } from '@mui/material';
+import { Close } from '@mui/icons-material';
 
 import Lobby from '../components/Lobby';
 import ShowQuestion from '../components/ShowQuestion';
 import Final from '../components/Final';
+import api from '../services/api';
 
-export default function QuizApply () {
+export default function QuizApply() {
+  const navigate = useNavigate();
+
   const [quizStage, setQuizStage] = useState('lobby'); // 'lobby', 'question', 'podium'
-  
+  const [open, setOpen] = useState(false);
+
   const startQuiz = () => {
     setQuizStage('question');
   };
@@ -19,14 +26,58 @@ export default function QuizApply () {
     setQuizStage('')
   }
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleCancel = async () => {
+    await api.get('/limparEstado').then(response => {
+      navigate('/');
+    });
+  }
+
   return (
-    <div>
+    <Container>
+      <Container style={{ display: 'flex', justifyContent: 'flex-end', width: '90%' }}>
+        <IconButton aria-label='Cancelar questionário' color="error" size='large' onClick={handleClickOpen}>
+          <Close sx={{ fontSize: '2.5rem' }} />
+        </IconButton>
+      </Container>
+
       {quizStage === 'lobby' && <Lobby onStartQuiz={startQuiz} />}
-      
-      {quizStage === 'question' && ( <ShowQuestion onFinishQuiz={finishQuiz} /> )}
-      
-      {quizStage === 'final' && <Final onFinishResults={finishApply}/>}
-    </div>
+
+      {quizStage === 'question' && (<ShowQuestion onFinishQuiz={finishQuiz} />)}
+
+      {quizStage === 'final' && <Final onFinishResults={finishApply} />}
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Tem certeza?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Cancelar aplicação do questionário?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Não, continuar questionário</Button>
+          <Button onClick={handleCancel} color="error">
+            Sim, cancelar questionário
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+    </Container>
   );
 };
 
