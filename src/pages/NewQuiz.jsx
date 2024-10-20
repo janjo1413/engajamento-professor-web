113e0cad0e70314249bb10cb803717ba459e596d
Guilherme Santos <santosgui678@gmail.com>
2024-10-20 12:05:33 -0300

    Adicionado cadastro de questionario

--- src/pages/NewQuiz.jsx
+++ src/pages/NewQuiz.jsx
@@ -1,16 +1,28 @@
-import { Box, Button, Container, FormControl, FormLabel, Grid, OutlinedInput } from "@mui/material";
+import { Alert, Box, Button, Container, FormControl, FormLabel, Grid, OutlinedInput, Snackbar } from "@mui/material";
 import { Save } from '@mui/icons-material';
 import { useState } from "react";
 import { read, utils } from 'xlsx';
 import QuestionCard from "../components/QuestionCard";
-
-const saveQuiz = () => {
-    console.log('SALVEI O QUIZ');
-}
+import api from "../services/api";
+import { useNavigate } from "react-router-dom";
 
 export default function NewQuiz() {
-    const [data, setData] = useState(null);
+    const [quizName, setQuizName] = useState('');
+    const [quizDescription, setQuizDescription] = useState('');
+    const [quizData, setQuizData] = useState(null);
     const [visible, setVisible] = useState(false);
+    const [error, setError] = useState(false);
+    const [success, setSuccess] = useState(false);
+
+    const navigate = useNavigate();
+    
+    const handleCloseMessage = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setError(false);
+    }
 
     const handleFileUpload = (e) => {
         const file = e.target.files[0];
@@ -23,11 +35,49 @@ export default function NewQuiz() {
 
             const sheetData = utils.sheet_to_json(sheet, { header: 1 });
 
-            setData(sheetData);
+            setQuizData(sheetData);
         };
 
         reader.readAsBinaryString(file);
     };
+
+
+    const saveQuiz = async () => {
+        if(!quizName || !quizDescription || !quizData){
+            setError(true);
+            return;
+        }
+
+        const savedQuestions = quizData.map(question => {
+            return {
+                enunciado: question[0],
+                resposta: question[1],
+                tema: question[2]
+            };
+        });
+
+
+        await api.post('/cadastraQuestionario', JSON.stringify({
+            questionario: {
+                nome: quizName,
+                descricao: quizDescription,
+                questoes: savedQuestions
+            }
+        }))
+            .then(function (response) {
+                setSuccess(true);
+
+                setQuizName('');
+                setQuizDescription('');
+                setQuizData(null);
+
+                navigate('/');
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+    }
+
 
     return (
         <div>
@@ -37,14 +87,38 @@ export default function NewQuiz() {
                 <Grid item xs={4}>
                     <FormControl>
                         <FormLabel htmlFor="quiz">Nome do questionário</FormLabel>
-                        <OutlinedInput id="quiz" name="quiz" type="quiz" placeholder="Nome do questionário" aria-describedby="Nome do questionário" required size="medium" />
+                        <OutlinedInput
+                            id="quiz"
+                            name="quiz"
+                            type="quiz"
+                            placeholder="Nome do questionário"
+                            aria-describedby="Nome do questionário"
+                            required
+                            size="medium"
+                            value={quizName}
+                            onChange={(event) => {
+                                setQuizName(event.target.value);
+                            }}
+                        />
                     </FormControl>
                 </Grid>
 
                 <Grid item xs={4}>
                     <FormControl>
                         <FormLabel htmlFor="description">Descrição</FormLabel>
-                        <OutlinedInput id="description" name="description" type="description" placeholder="Descrição do questionário" aria-describedby="nome-da-turma" required size="medium" />
+                        <OutlinedInput
+                            id="description"
+                            name="description"
+                            type="description"
+                            placeholder="Descrição do questionário"
+                            aria-describedby="nome-da-turma"
+                            required
+                            size="medium"
+                            value={quizDescription}
+                            onChange={(event) => {
+                                setQuizDescription(event.target.value);
+                            }}
+                        />
                     </FormControl>
                 </Grid>
 
@@ -54,7 +128,7 @@ export default function NewQuiz() {
                         accept=".xls, .xlsx, .ods, .csv"
                         onChange={handleFileUpload} />
 
-                    {data && (
+                    {quizData && (
                         <Container sx={{ my: 4 }}>
 
                             <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '35%' }}>
@@ -72,15 +146,15 @@ export default function NewQuiz() {
                             {
                                 visible && (
                                     <>
-                            <h3>Número de questões: {data.length}</h3>
-                            <Grid container spacing={4}>
+                                        <h3>Número de questões: {quizData.length}</h3>
+                                        <Grid container spacing={4}>
 
-                                {data.map((item, index) => (
-                                    <QuestionCard key={index} question={item[0]} answer={item[1]} subject={item[2]} />
-                                ))}
+                                            {quizData.map((item, index) => (
+                                                <QuestionCard key={index} question={item[0]} answer={item[1]} subject={item[2]} />
+                                            ))}
 
-                            </Grid>
-</>
+                                        </Grid>
+                                    </>
                                 )
                             }
 
@@ -96,8 +170,31 @@ export default function NewQuiz() {
                         Salvar Questionário
                     </Button>
                 </Grid>
-
             </Grid>
+
+            <Snackbar open={error} autoHideDuration={3000} onClose={handleCloseMessage}
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+                <Alert
+                    onClose={handleCloseMessage}
+                    severity="error"
+                    variant="filled"
+                    sx={{ width: '100%' }}
+                >
+                    Nome, descrição e questões são obrigatórios
+                </Alert>
+            </Snackbar>
+
+            <Snackbar open={success} autoHideDuration={3000} onClose={handleCloseMessage}
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+                <Alert
+                    onClose={handleCloseMessage}
+                    severity="success"
+                    variant="filled"
+                    sx={{ width: '100%' }}
+                >
+                    Questionário cadastrado com sucesso
+                </Alert>
+            </Snackbar>
 
         </div>
     )
