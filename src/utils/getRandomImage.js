dd911c562447d7f92bfb72e1d51018165f98b90f
Guilherme Santos <santosgui678@gmail.com>
2024-11-07 14:58:03 -0300

    Ajustes nas imagens

--- src/utils/getRandomImage.js
+++ src/utils/getRandomImage.js
@@ -13,12 +13,10 @@ import class3 from '../assets/class3.jpeg';
 import class4 from '../assets/class4.jpeg';
 import class5 from '../assets/class5.jpeg';
 import class6 from '../assets/class6.jpeg';
-import class7 from '../assets/class7.jpeg';
-import class8 from '../assets/class8.jpeg';
 
 const getRandomImage = (type) => {
     const quizImages = [quiz1, quiz2, quiz3, quiz4, quiz5, quiz6, quiz7, quiz8];
-    const classImages = [class1, class2, class3, class4, class5, class6, class7, class8];
+    const classImages = [class1, class2, class3, class4, class5, class6];
 
     if(type === 'quiz'){
         const randomIndex = Math.floor(Math.random() * quizImages.length);
