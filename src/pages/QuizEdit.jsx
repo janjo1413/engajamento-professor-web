1b8ad8536b0a0c696525d73a4e6fd6d5d8175885
Guilherme Santos <santosgui678@gmail.com>
2024-11-18 11:16:48 -0300

    Adicionados componentes da tela de edicao

--- src/pages/QuizEdit.jsx
+++ src/pages/QuizEdit.jsx
@@ -1,17 +1,26 @@
 import { useEffect, useState } from "react";
-import { CircularProgress, Container, Grid, Typography } from "@mui/material";
+import { CircularProgress, Container, Grid, TextField, Typography } from "@mui/material";
 import api from "../services/api";
 import { useQuizClass } from "../contexts/QuizClassContext";
+import QuestionCard from "../components/QuestionCard";
 
 export default function QuizEdit() {
-    const [selectedQuiz, setSelectedQuiz] = useState(null);
+    //const [selectedQuiz, setSelectedQuiz] = useState(null);
     const { quizCode } = useQuizClass();
+
+    const [quizName, setQuizName] = useState(null);
+    const [description, setDescription] = useState(null);
+    const [questions, setQuestions] = useState(null);
 
     useEffect(() => {
         async function getQuiz() {
             await api.post('/carregaQuestionario', JSON.stringify({ codigoQuestionario: quizCode }))
                 .then(response => {
-                    setSelectedQuiz(response.data)
+                    const selectedQuiz = response.data;
+
+                    setQuizName(selectedQuiz.nome)
+                    setDescription(selectedQuiz.descricao)
+                    setQuestions(selectedQuiz.questoes)
                 })
                 .catch(error => console.error(error))
         }
@@ -22,14 +31,35 @@ export default function QuizEdit() {
     return (
         <Container maxWidth="lg" sx={{ mt: 2 }}>
             {
-                !selectedQuiz ? (
+                !(quizName || description || questions) ? (
                     <CircularProgress />
                 ) : (
                     <Grid container spacing={2}>
                         <Grid item xs={12}>
                             <Typography variant="h4" component="div" sx={{ mb: 2 }}>
-                                {selectedQuiz.nome}
+                                {quizName}
                             </Typography>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <TextField
+                                id="description"
+                                label="Descrição do questionário"
+                                variant="outlined"
+                                required
+                                fullWidth
+                                value={description}
+                                onChange={(event) => {
+                                    setDescription(event.target.value);
+                                }}
+                                sx={{ width: '50%' }}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12} sx={{ mb: 10 }}>
+                            {questions.map((item, index) => (
+                                <QuestionCard key={index} question={item.enunciado} subject={item.tema} answer={item.resposta} />
+                            ))}
                         </Grid>
                     </Grid>
                 )
